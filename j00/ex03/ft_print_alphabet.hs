574dd02fee88895058e655d0966e31935c892e90
Léonard Marques <lmarques@student.42.fr>
2018-01-22 19:52:42 +0100

    adde j00 ex03

---
+++ j00/ex03/ft_print_alphabet.hs
@@ -0,0 +1,11 @@
+import Data.Char
+
+ft_print_alphabet () = print_char 'a' where
+    print_char c =
+        if (ord c <= ord 'z') then do
+            putStr (c:[])
+            print_char (chr ((ord c) + 1))
+        else putStr ("\n")
+
+main =
+    ft_print_alphabet ()
