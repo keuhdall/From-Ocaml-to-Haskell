216c1ff0811e18c5e719ace0441d7d4907fba4af
Léonard Marques <lmarques@student.42.fr>
2018-01-22 18:59:53 +0100

    added j00 ex02

---
+++ j00/ex02/ft_power.hs
@@ -0,0 +1,11 @@
+ft_power n pow = 
+    if (n == 0) then 0
+    else do
+        if (pow == 0) then 1
+        else n * (ft_power n (pow - 1))
+
+main = do
+    print (ft_power 5 5)
+    print (ft_power 0 5)
+    print (ft_power 5 0)
+    print (ft_power 3 2)
