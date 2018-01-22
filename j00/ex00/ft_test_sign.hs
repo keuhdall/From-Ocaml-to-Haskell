d2cf583b4dbc2ea6e427f5223696bbac5c835bb8
Léonard Marques <lmarques@student.42.fr>
2018-01-22 19:55:38 +0100

    these functions no longer use print

--- j00/ex00/ft_test_sign.hs
+++ j00/ex00/ft_test_sign.hs
@@ -1,4 +1,4 @@
-ft_test_sign x = if (x < 0) then print "negatve" else print "positive"
+ft_test_sign x = if (x < 0) then putStrLn "negatve" else putStrLn "positive"
 
 main = do
     ft_test_sign 5
--- j00/ex01/ft_countdown.hs
+++ j00/ex01/ft_countdown.hs
@@ -1,13 +1,13 @@
 ft_countdown x =
     if (x > 0) then do
-        print x
+        putStrLn x
         ft_countdown (x - 1)
     else
-        print 0
+        putStrLn 0
 
 main = do
     ft_countdown 5
-    print "-----"
+    putStrLn "-----"
     ft_countdown 0
-    print "-----"
+    putStrLn "-----"
     ft_countdown (-2)
