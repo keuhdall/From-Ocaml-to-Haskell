946463e1458b0f4311ddf2380d133de140fa8db1
Léonard Marques <lmarques@student.42.fr>
2018-01-23 01:06:09 +0100

    finished j00 ex04

--- j00/ex04/ft_print_comb.hs
+++ j00/ex04/ft_print_comb.hs
@@ -1,16 +1,17 @@
 import Text.Printf
 
 ft_print_comb () = print_nums 0 1 2 where
-    print_nums :: Int -> Int -> Int -> Int
     print_nums x y z = do
-        printf "%d %d %d" x y z
-        if (x < 7) then do
+        putStr $ show x
+        putStr $ show y
+        putStr $ show z
+        if (x < 7) then do  
             putStr ", "
             if (z < 9) then print_nums x y (z + 1)
             else if (y < 8) then print_nums x (y + 1) (y + 2)
             else if (x < 7) then print_nums (x + 1) (x + 2) (x + 3)
             else do putStr ""
-        else do putStr ("\n")
+        else do putStr "\n"
 
 main = do
     ft_print_comb ()
