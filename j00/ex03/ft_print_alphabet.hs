3ff315c26af760c390202665fe0a622a28ee2952
Léonard Marques <lmarques@student.42.fr>
2018-01-23 01:12:44 +0100

    removed useless parantheses in ex03

--- j00/ex03/ft_print_alphabet.hs
+++ j00/ex03/ft_print_alphabet.hs
@@ -5,7 +5,7 @@ ft_print_alphabet () = print_char 'a' where
         if (ord c <= ord 'z') then do
             putStr (c:[])
             print_char (chr ((ord c) + 1))
-        else putStr ("\n")
+        else putStr "\n"
 
 main =
     ft_print_alphabet ()
