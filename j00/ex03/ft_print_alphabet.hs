a874fadff754d80974f49c61015f68ac1f728a66
Léonard Marques <lmarques@student.42.fr>
2018-01-23 16:31:36 +0100

    simplified j00 ex03

--- j00/ex03/ft_print_alphabet.hs
+++ j00/ex03/ft_print_alphabet.hs
@@ -2,9 +2,9 @@ import Data.Char
 
 ft_print_alphabet () = print_char 'a' where
     print_char c =
-        if (ord c <= ord 'z') then do
+        if (c <= 'z') then do
             putStr (c:[])
-            print_char (chr ((ord c) + 1))
+            print_char $ chr $ (ord c) + 1
         else putStr "\n"
 
 main =
