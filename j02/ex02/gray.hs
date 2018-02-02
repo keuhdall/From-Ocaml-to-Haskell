5956f4335ea612a7e2f1ba71d623dadeb0ded0c7
Léonard Marques <lmarques@student.42.fr>
2018-02-02 16:07:20 +0100

    still working on j02 ex02

--- j02/ex02/gray.hs
+++ j02/ex02/gray.hs
@@ -1,34 +1,47 @@
-print_lst l = case l of
-    []      -> ()
+print_int n = putStr $ show n
+
+concat_lst l1 l2 = case l1 of
+    (hd:tl) -> hd:(concat_lst tl l2)
+    []      -> l2
+
+reverse_lst l = rev_loop l [] where
+    rev_loop l new = case l of
+        (hd:tl) -> rev_loop tl (concat_lst [hd] new)
+        []      -> new
+
+prefix_lst prefix l =
+    concat_lst [prefix] l
+
+lst_map f l = map_loop l [] where
+    map_loop l new = case l of
+        (hd:tl) -> map_loop tl (concat_lst new [f hd])
+        []      -> new
+
+lst_cover f l = case l of
     (hd:tl) -> do
-        putStr (head ++ " ")
-        print_lst tail
+        f hd
+        lst_cover f tl
+    []      -> IO ()
 
-prepend c s =
-    let s' = Bytes.create (String.length s + 1) in
-    s'.[0] <- c;
-    String.blit s 0 s' 1 (String.length s);
-    s'
+print_lst f seq = case seq of
+    (hd:sc:tl)  -> do
+        f print_int head
+        putStr " "
+        print_lst f (sc:tl)
+    (hd:tl)     -> do
+        f print_int hd
+        print_lst f tl
+    []          -> IO ()
 
-concat x y = case (x, y) of
-    ([], [])    -> []
-    ([], _)     -> y
-    (_,[])      -> x
-    (hd:tl, _)  -> hd:(concat tl y)
-
-list_map f l = case l of
-    []      -> []
-    (hd:tl) -> (f hd):(list_map f tl)
-
-gray n =
-    if (n <= 1) then 
-        ["0"; "1"]
-    else
-        do
-            let g = gray (n - 1) in
-            concat (list_map (prepend '0') g) (list_map (prepend '1') g)
+gray n = gray_loop n [[]] where
+    gray_loop n seq_ret =
+        if (n <= 0) then seq_ret
+        else gray_loop (n - 1) (concat_lst (lst_map (prefix_lst 0) seq_ret) (lst_map
+            (prefix_lst 1) (reverse_lst seq_ret)))
 
 main = do
+    print_lst $ lst_cover $ gray 42
+{-main = do
     putStrLn "Test with [3] : "
     print_lst $ gray 3
     putStrLn "\nTest with [2] : "
@@ -36,4 +49,4 @@ main = do
     putStrLn "\nTest with [1] : "
     print_lst $ gray 1
     putStrLn "Test with [5] : "
-    print_lst $ gray 5
+    print_lst $ gray 5-}
