e8cc6651b7d9e35792ece57952a345c495f74e89
Léonard Marques <lmarques@student.42.fr>
2018-02-06 18:20:21 +0100

    corrected Card module

--- j04/ex02/Card.hs
+++ j04/ex02/Card.hs
@@ -1,74 +1,79 @@
 module Card(T(), newCard, allSpades, allHearts, allDiamonds, allClubs, all, getValue, getColor, toString,
     toStringVerbose, compare, max, min, best, isOf, isSpade, isHeart, isDiamond, isClub) where
-    import Prelude hiding (all, max, min)
+    import Prelude hiding (all, max, min, compare)
     import qualified Color
     import qualified Value
 
     data T = T {
         color :: Color.T,
         value :: Value.T
-    } -- deriving (Show)
+    }
 
     newCard newColor newValue = T { color=newColor, value=newValue }
 
-    allSpades = fillAllSpades [] T2 where
+    allSpades :: [T]
+    allSpades = fillAllSpades [] Value.T2 where
+        fillAllSpades :: [T] -> Value.T -> [T]
         fillAllSpades l t =
-            if (t == As) then l ++ [(T { color=Spade, value=As })]
+            if (t == Value.As) then l ++ [(T { color=Color.Spade, value=Value.As })]
             else do
-                l ++ [(T { color=Spade, value=t})]
-                fillAllSpades l Value.next(t)
+                l ++ [(T { color=Color.Spade, value=t})]
+                fillAllSpades l (Value.next t)
 
-    allHearts = fillAllHearts [] T2 where
+    allHearts :: [T]
+    allHearts = fillAllHearts [] Value.T2 where
         fillAllHearts l t =
-            if (t == As) then l ++ [(T { color=Heart, value=As })]
+            if (t == Value.As) then l ++ [(T { color=Color.Heart, value=Value.As })]
             else do
-                l ++ [(T { color=Heart, value=t})]
-                fillAllHearts l Value.next(t)
+                l ++ [(T { color=Color.Heart, value=t})]
+                fillAllHearts l (Value.next t)
 
-    allDiamonds = fillAllDiamonds [] T2 where
+    allDiamonds :: [T]
+    allDiamonds = fillAllDiamonds [] Value.T2 where
         fillAllDiamonds l t =
-            if (t == As) then l ++ [(T { color=Diamond, value=As })]
+            if (t == Value.As) then l ++ [(T { color=Color.Diamond, value=Value.As })]
             else do
-                l ++ [(T { color=Diamond, value=t})]
-                fillAllDiamonds l Value.next(t)
+                l ++ [(T { color=Color.Diamond, value=t})]
+                fillAllDiamonds l (Value.next t)
 
-    allClubs = fillAllClubs [] T2 where
+    allClubs :: [T]
+    allClubs = fillAllClubs [] Value.T2 where
         fillAllClubs l t =
-            if (t == As) then l ++ [(T { color=Club, value=As })]
+            if (t == Value.As) then l ++ [(T { color=Color.Club, value=Value.As })]
             else do
-                l ++ [(T { color=Club, value=t})]
-                fillAllClubs l Value.next(t)
+                l ++ [(T { color=Color.Club, value=t})]
+                fillAllClubs l (Value.next t)
     
     all = allSpades ++ allHearts ++ allDiamonds ++ allClubs
 
     getColor (T color _) = color
     getValue (T _ value) = value
 
-    toString t = (Value.toString t.value) ++ (Color.toString t.color)
-    toStringVerbose t = "Card(" ++ (Value.toStringVerbose t.value) ++ ", " ++ (Color.toStringVerbose t.color) ++ ")"
+    toString t = (Value.toString (value t)) ++ (Color.toString (color t))
+    toStringVerbose t = "Card(" ++ (Value.toStringVerbose (value t)) ++ ", " ++ (Color.toStringVerbose (color t)) ++ ")"
 
     compare t1 t2
-        |   (Value.toInt (t1.value) < Value.toInt (t2.value)) = (-1)
-        |   (Value.toInt (t1.value) > Value.toInt (t2.value)) = 1
+        |   (Value.toInt (value t1) < Value.toInt (value t2)) = (-1)
+        |   (Value.toInt (value t1) > Value.toInt (value t2)) = 1
         |   otherwise = 0
 
     min t1 t2
-        |   (Value.toInt t1.value > Value.toInt t2.value) = t2
+        |   (Value.toInt (value t1) > Value.toInt (value t2)) = t2
         |   otherwise = t1
 
     max t1 t2
-        |   (Value.toInt t1.value >= Value.toInt t2.value) = t1
+        |   (Value.toInt (value t1) >= Value.toInt (value t2)) = t1
         |   otherwise = t2
     
     best l = case l of
         (hd:tl) ->  foldl max hd tl
         []      ->  error "Error"
     
-    isOf t color
-        |   (color t) == color  = True
-        |   otherwise           = False
+    isOf t c
+        |   (color t) == c  = True
+        |   otherwise       = False
 
-    isSpade     t = isOf t Spade
-    isHeart     t = isOf t Heart
-    isDiamond   t = isOf t Diamond
-    isClub      t = isOf t Club
+    isSpade     t = isOf t Color.Spade
+    isHeart     t = isOf t Color.Heart
+    isDiamond   t = isOf t Color.Diamond
+    isClub      t = isOf t Color.Club
