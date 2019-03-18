cf627e8a9f2cc9466110268a3c1b28882e2bc1cb
Léonard Marques <lmarques@student.42.fr>
2019-03-18 06:14:47 +0100

    added missing ex04

---
+++ j01/ex04/fibonacci.hs
@@ -0,0 +1,12 @@
+fibonacci :: Int -> Int
+fibonacci n
+    | n < 0 = -1
+    | n == 0 = 0
+    | n == 1 = 1
+    | otherwise = fibonacci (n-2) + fibonacci (n-1)
+
+main = do
+    putStrLn $ show $ fibonacci (-42)
+    putStrLn $ show $ fibonacci 1
+    putStrLn $ show $ fibonacci 3
+    putStrLn $ show $ fibonacci 6
